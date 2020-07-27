8b7a0ddd5ee55073037702b165f6e28d517f6a39
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-27 20:36:09 +0200

    LOCAL HACKS

---
+++ _test.sh
@@ -0,0 +1,46 @@
+
+TESTDIR=/xs
+MODULE=srvfs
+FSNAME=srvfs
+
+set -e
+
+do_unmount() {
+	if mountpoint $TESTDIR >/dev/null; then
+		echo "unmounting"
+		umount $TESTDIR
+	else
+		echo "not mounted"
+	fi
+}
+
+do_unload() {
+	if cat /proc/modules | grep "$MODULE " >/dev/null; then
+		echo "unloading module"
+		rmmod $MODULE
+	else
+		echo "module not loaded"
+	fi
+}
+
+do_load() {
+	if cat /proc/modules | grep "$MODULE " >/dev/null; then
+		echo "module already loaded"
+	else
+		echo "loading module"
+		insmod kernel/$MODULE.ko
+	fi
+}
+
+do_mount() {
+	mkdir -p $TESTDIR
+	mount none $TESTDIR -t $FSNAME
+}
+
+do_list() {
+	ls -la $TESTDIR
+}
+
+log_info() {
+	echo "INFO: $*" >&2
+}
