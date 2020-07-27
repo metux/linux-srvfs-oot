2820752fbf2f64793551cccdd2ac5b9cd023adb8
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-27 22:14:16 +0200

    1

--- srvfs-main.c
+++ srvfs-main.c
@@ -4,6 +4,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 #include <asm/atomic.h>
 #include <asm/uaccess.h>
 
@@ -15,12 +16,14 @@ MODULE_AUTHOR("mtx");
 
 #define SRVFS_MAGIC 0x19980123
 
-/*
- * Implement an array of counters.
- */
 #define NCOUNTERS 4
 static atomic_t counters[NCOUNTERS];
 
+
+struct srvfs_inode {
+	atomic_t counter;
+	int mode;
+};
 
 
 /*
@@ -147,31 +150,42 @@ struct tree_descr OurFiles[] = {
  * that looks like a filesystem to work with.
  */
 
-static int srvfs_create_file (struct super_block *sb, struct dentry *root, struct tree_descr *files, int idx)
+static int srvfs_create_file (struct super_block *sb, struct dentry *root, const char* name, int idx)
 {
 	struct dentry *dentry;
 	struct inode *inode;
+	struct srvfs_inode *priv;
 
-	dentry = d_alloc_name(root, files->name);
+	priv = kmalloc(sizeof(struct srvfs_inode), GFP_KERNEL);
+	if (!priv) {
+		pr_err("srvfs_create_file(): failed to malloc inode priv\n");
+		goto err;
+	}
+
+	atomic_set(&priv->counter, 0);
+
+	dentry = d_alloc_name(root, name);
 	if (!dentry)
-		goto out;
+		goto err;
 	inode = new_inode(sb);
 	if (!inode) {
 		dput(dentry);
-		goto out;
+		goto err;
 	}
 	inode->i_mode = S_IFREG | S_IWUSR | S_IRUGO;
 	inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
 	inode->i_fop = &srvfs_file_ops;
 	inode->i_ino = idx;
+
 	d_add(dentry, inode);
-	return 0;
-out:
 	return 1;
+// FIXME: release resources
+err:
+	return 0;
 }
 
 static const struct super_operations simple_super_operations = {
-        .statfs         = simple_statfs,
+	.statfs	= simple_statfs,
 };
 
 /*
@@ -179,54 +193,56 @@ static const struct super_operations simple_super_operations = {
  */
 static int srvfs_fill_super (struct super_block *sb, void *data, int silent)
 {
-    struct inode *inode;
-    struct dentry *root;
-    int i;
-    struct tree_descr *files = OurFiles;
+	struct inode *inode;
+	struct dentry *root;
+	int i;
+	struct tree_descr *files = OurFiles;
 
-    sb->s_blocksize = PAGE_SIZE;
-    sb->s_blocksize_bits = PAGE_SHIFT;
-    sb->s_magic = SRVFS_MAGIC;
-    sb->s_op = &simple_super_operations;
-    sb->s_time_gran = 1;
+	sb->s_blocksize = PAGE_SIZE;
+	sb->s_blocksize_bits = PAGE_SHIFT;
+	sb->s_magic = SRVFS_MAGIC;
+	sb->s_op = &simple_super_operations;
+	sb->s_time_gran = 1;
 
-    inode = new_inode(sb);
-    if (!inode)
-	return -ENOMEM;
-    /*
-     * because the root inode is 1, the files array must not contain an
-     * entry at index 1
-     */
-    inode->i_ino = 1;
-    inode->i_mode = S_IFDIR | 0755;
-    inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
-    inode->i_op = &simple_dir_inode_operations;
-    inode->i_fop = &simple_dir_operations;
-    set_nlink(inode, 2);
-    root = d_make_root(inode);
-    if (!root)
-	return -ENOMEM;
+	inode = new_inode(sb);
+	if (!inode)
+		return -ENOMEM;
+	/*
+	 * because the root inode is 1, the files array must not contain an
+	 * entry at index 1
+	 */
+	inode->i_ino = 1;
+	inode->i_mode = S_IFDIR | 0755;
+	inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+	inode->i_op = &simple_dir_inode_operations;
+	inode->i_fop = &simple_dir_operations;
+	set_nlink(inode, 2);
+	root = d_make_root(inode);
+	if (!root) {
+		pr_info("fill_super(): could not create root\n");
+		return -ENOMEM;
+	}
 
-    for (i = 0; !files->name || files->name[0]; i++, files++) {
-	if (!files->name)
-	    continue;
+	for (i = 0; !files->name || files->name[0]; i++, files++) {
+		if (!files->name)
+			continue;
 
-	/* warn if it tries to conflict with the root inode */
-	if (unlikely(i == 1))
-	    printk(KERN_WARNING "%s: %s passed in a files array"
-		"with an index of 1!\n", __func__,
-		sb->s_type->name);
+		/* warn if it tries to conflict with the root inode */
+		if (unlikely(i == 1))
+			printk(KERN_WARNING "%s: %s passed in a files array"
+				"with an index of 1!\n", __func__,
+			sb->s_type->name);
 
-	if (!srvfs_create_file(sb, root, files, i))
-		goto out;
-    }
-    sb->s_root = root;
-    return 0;
+		if (!srvfs_create_file(sb, root, files->name, i))
+			goto out;
+	}
+	sb->s_root = root;
+	return 0;
 out:
-    d_genocide(root);
-    shrink_dcache_parent(root);
-    dput(root);
-    return -ENOMEM;
+	d_genocide(root);
+	shrink_dcache_parent(root);
+	dput(root);
+	return -ENOMEM;
 }
 
 /*
