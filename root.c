95bccc32d9a6476b61c0c006d3f90b482c300355
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-28 15:39:52 +0200

    1

--- root.c
+++ root.c
@@ -31,17 +31,7 @@ static int srvfs_dir_unlink(struct inode *inode, struct dentry *dentry)
 
 static int srvfs_dir_create (struct inode *inode, struct dentry *dentry, umode_t mode, bool excl)
 {
-	int ret;
-	pr_info("srvfs_dir_create(): trying to create dir entry\n");
-	if (excl)
-		pr_info("srvfs_dir_create() exclusive\n");
-	else
-		pr_info("srvfs_dir_create() not exclusive\n");
-
-	ret = srvfs_insert_file(inode->i_sb, dget(dentry));
-	pr_info("srvfs_dir_create() returned: %d\n", ret);
-
-	return ret;
+	return srvfs_insert_file(inode->i_sb, dget(dentry));
 }
 
 const struct inode_operations srvfs_rootdir_inode_operations = {
